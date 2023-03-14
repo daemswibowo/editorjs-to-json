557a3f5d51d15f7bde9fef8f9672ac950096c51b
Dimas Wibowo <dimas@karyakarsa.com>
2023-03-14 12:49:35 +0700

    import tools from constants

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,22 +1,5 @@
 import {createReactEditorJS} from "react-editor-js";
-import dynamic from "next/dynamic";
-
-const Header = dynamic(() => import('@editorjs/header'), {ssr: false});
-const Link = dynamic(() => import('@editorjs/link'), {ssr: false});
-const Embed = dynamic(() => import('@editorjs/embed'), {ssr: false});
-
-export const EDITOR_TOOLS = {
-    link: Link,
-    header: Header,
-    embed: {
-        class: Embed,
-        config: {
-            services: {
-                youtube: true,
-            }
-        }
-    },
-}
+import {EDITOR_TOOLS} from "@/utils/constants";
 
 const Editor = (props) => {
     const {...rest} = props;
